b1c5c6d928d00d467e01e81b6b9a7a70dea0ee7b
ben <bencrane5.12@gmail.com>
2019-01-21 13:52:27 -0500

    Update keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://admin:fTxUbQoYBmPw4Xv7@maindb-kwj5s.mongodb.net/main?retryWrites=true",
+  mongoURI: "mongodb+srv://admin:tPHCuh0XUqZF7QPd@maindb-kwj5s.mongodb.net/main?retryWrites=true",
   secretOrKey: "superSecretKey"
 }
