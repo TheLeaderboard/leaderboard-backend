527e5b1737322c486f2c71555b71c9e7f2fc1989
ben <bencrane5.12@gmail.com>
2019-01-29 22:41:02 -0500

    Add JWT middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+
+function checkToken(req, res, next) {
+  let token = req.headers["x-access-token"] || req.headers["authorization"] || "";
+  if (token.startsWith("Bearer ")) {
+    // remove bearer from string
+    token = token.slice(7, token.length);
+  }
+
+  if (token) {
+    jwt.verify(token, process.env.SECRET_OR_KEY, (err, decoded) => {
+      if (err) {
+        return res.json({
+          success: false,
+          message: "Token is not valid"
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.json({
+      success: false,
+      message: "Auth token is not supplied"
+    });
+  }
+};
+
+module.exports = {
+  checkToken: checkToken
+}
